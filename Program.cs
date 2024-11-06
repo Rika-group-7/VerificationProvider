a4369ab5a742e85993bc74474d18d28d56071bc5
Rebecca <rebeccalagerr@outlook.com>
2024-11-06 11:46:15 +0100

    Added context

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+
+        var configuration = context.Configuration;
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? Environment.GetEnvironmentVariable("DefaultConnection");
     })
     .Build();
 
